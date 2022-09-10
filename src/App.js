1a3bd10dacab9bb2a355293cec3d5d1ead5e49a6
Ioveeel <iviko888@gmail.com>
2022-09-10 17:24:21 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.scss';
+
+//Pages
+import { Home,Contact } from './pages'
+//Components
+import { Header,Footer } from './components'
+
+function App() {
+ 
+  return (
+  <>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/contact' element={<Contact/>} />
+    
+
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
+  </>
+  );
+}
+
+export default App;
